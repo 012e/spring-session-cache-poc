162fb4471c9fd1c4002c6ba514ca489f473bf427
012e <huyphmnhat@gmail.com>
2024-09-27 23:22:51 +0700

    log about session storage type

--- session-auth-db/src/main/java/com/u012e/session_auth_db/SessionAuthDbApplication.java
+++ session-auth-db/src/main/java/com/u012e/session_auth_db/SessionAuthDbApplication.java
@@ -1,13 +1,25 @@
 package com.u012e.session_auth_db;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
-public class SessionAuthDbApplication {
+@SpringBootApplication
+@Slf4j
+public class SessionAuthDbApplication implements ApplicationRunner {
+	@Value("${session.storage:database-only}")
+	private String sessionStorage;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SessionAuthDbApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		log.info("Supported session storage: database-only, redis-only, database-redis");
+		log.info("Session storage: {}", sessionStorage);
+	}
 }
