0b3c9f1b08ea27f2c3906173b57b68d1ea744f42
012e <huyphmnhat@gmail.com>
2024-11-03 16:29:18 +0700

    use CrudRespository instead of JpaRepository

--- src/main/java/com/u012e/session_auth_db/controller/HelloWorldController.java
+++ src/main/java/com/u012e/session_auth_db/controller/HelloWorldController.java
@@ -1,15 +1,10 @@
 package com.u012e.session_auth_db.controller;
 
-import com.u012e.session_auth_db.dto.UserDto;
 import com.u012e.session_auth_db.service.UserService;
 import com.u012e.session_auth_db.utils.GenericResponse;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
--- src/main/java/com/u012e/session_auth_db/repository/DbSessionRepository.java
+++ src/main/java/com/u012e/session_auth_db/repository/DbSessionRepository.java
@@ -2,14 +2,14 @@ package com.u012e.session_auth_db.repository;
 
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.u012e.session_auth_db.model.Session;
 
 import java.util.Optional;
 
 @Repository
-public interface DbSessionRepository extends JpaRepository<Session, Long> {
+public interface DbSessionRepository extends CrudRepository<Session, Long> {
     Optional<Session> findByToken(String token);
-    @Transactional
     Long deleteByToken(String token);
 }
