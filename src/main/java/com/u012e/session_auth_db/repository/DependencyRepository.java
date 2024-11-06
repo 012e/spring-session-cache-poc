b2f9fbbb1b42d6ca235e29e9f8fc4ceb507b70f0
Pham Nhat Huy <huyphmnhat@gmail.com>
2024-11-06 21:36:30 +0700

    Generic argument misplaced (#6)

--- src/main/java/com/u012e/session_auth_db/repository/DependencyRepository.java
+++ src/main/java/com/u012e/session_auth_db/repository/DependencyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DependencyRepository extends CrudRepository<Long, Dependency> {
+public interface DependencyRepository extends CrudRepository<Dependency, Long> {
 }
--- src/main/java/com/u012e/session_auth_db/repository/SubjectRepository.java
+++ src/main/java/com/u012e/session_auth_db/repository/SubjectRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubjectRepository extends CrudRepository<Long, Subject> {
+public interface SubjectRepository extends CrudRepository<Subject, Long> {
 }
