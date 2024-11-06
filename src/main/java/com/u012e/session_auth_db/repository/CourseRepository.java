b2c921809af0cff457edab604421f8c35d08c44d
012e <huyphmnhat@gmail.com>
2024-11-06 21:35:34 +0700

    Generic argument misplaced

--- src/main/java/com/u012e/session_auth_db/repository/CourseRepository.java
+++ src/main/java/com/u012e/session_auth_db/repository/CourseRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends CrudRepository<Long, Course> {
+public interface CourseRepository extends CrudRepository<Course, Long> {
 }
